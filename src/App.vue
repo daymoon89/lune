6e4e00aa1365b65102f959e4a761f83d97d4ce02
daymoon89 <icn.daymoon@gmail.com>
2018-12-04 15:21:12 +0900

    Begin the Project

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <app-nav></app-nav>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import nav from '@/components/app-nav'
+
+export default {
+  name: 'app',
+  components: {
+    'app-nav': nav
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+div
+  width: 100%
+</style>
